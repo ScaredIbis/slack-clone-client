d72bdfbc4a6783eeb7694829ce239560b3398604
Chris Martin <chris.martin.95@hotmail.com>
2018-03-31 02:48:32 +1000

    initial commit - react setup with apollo graphql

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": 0,
+    "no-underscore-dangle": 0
+  },
+  "globals": {
+    "document": 1,
+    "window": 1
+  }
+};
